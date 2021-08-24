050a740faf3dc9ab18002012e52870fd6a2ab39a
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-08-24 16:54:45 +0900

    eslint 초기설정

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
 
 const app = express();
 dotenv.config();
