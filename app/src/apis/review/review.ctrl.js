cf28716dc351ae113a3d247af9a050a26b2d140c
김지수 <kimjisu3817@gmail.com>
2021-08-25 14:48:06 +0900

    Add(jisu/reivew):#22 	1. review api 폴더 생성. 	2. reivew models 폴더 생성. 	3. post 요청 양식 작성.

---
+++ app/src/apis/review/review.ctrl.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Review = require('../../models/services/Review/Review');
+
+const process = {
+  createByClubNum: (req, res) => {
+    // req: 별점, 내용.
+    // jwt 토큰의 id 값을 받아옴.
+    // members 테이블에서 해당하는 id의 club_no와 파라미터로 받은 clubnum이 같은지 확인.
+    // 같으면 후기 작성 가능.
+    // 작성자는 jwt 토큰 id값.
+    // 별점, 내용, 날짜가 저장되어야 함.
+    const review = new Review(req);
+    const response = review.createByClubNum();
+    return res.status(201).json(response);
+  },
+};
+
+module.exports = {
+  process,
+};
---
+++ app/src/models/services/Review/Review.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// const ReviewStorage = require('./ReviewStorage');
+
+class Review {
+  constructor(body) {
+    this.body = body;
+    this.params = params;
+  }
+
+  createByClubNum() {
+    const review = this.body;
+    console.log(review);
+  }
+}
+
+module.exports = Review;
