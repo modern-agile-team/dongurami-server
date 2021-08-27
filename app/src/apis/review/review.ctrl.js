2e1a065366740053ccad3332a500d8b452e66c10
김지수 <kimjisu3817@gmail.com>
2021-08-27 21:06:31 +0900

    Update(jisu/review): 주석 수정#22

--- app/src/apis/review/review.ctrl.js
+++ app/src/apis/review/review.ctrl.js
@@ -5,6 +5,7 @@ const ReviewStorage = require('../../models/services/Review/ReviewStorage');
 const Auth = require('../../models/services/Auth/Auth');
 
 const process = {
+  // 테스트 토큰
   createToken: (req, res) => {
     const token = Auth.createToken(req.body);
     if (!token) {
@@ -17,6 +18,7 @@ const process = {
       .json({ success: true, msg: 'JWT가 생성되었습니다.', token });
   },
 
+  // 후기 작성
   createByClubNum: async (req, res) => {
     const review = new Review(req);
     const response = await review.createByClubNum();
@@ -26,6 +28,7 @@ const process = {
     return res.status(400).json(response);
   },
 
+  // 모든 후기 리스트
   findAllReview: async (req, res) => {
     const reviewList = await ReviewStorage.findAllReview();
     return res.status(200).json(reviewList);
