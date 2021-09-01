9fcdf79ca73c1775ad47d839339ad43f228c4367
김지수 <kimjisu3817@gmail.com>
2021-09-01 15:18:43 +0900

    modify(jisu/review): 코드 컨벤션 수정

--- app/src/apis/review/index.js
+++ app/src/apis/review/index.js
@@ -6,10 +6,9 @@ const router = express.Router();
 const ctrl = require('./review.ctrl');
 
 // payload(JWT)와 params의 clubNum이 일치하는지 확인.
-router.post('/home/review', ctrl.process.createToken);
+router.post('/club/review', ctrl.process.createToken);
 // 동아리 부원이 맞다면 리뷰를 작성할 수 있는 API
-router.post('/home/review/:clubNum', ctrl.process.createByReview);
+router.post('/club/:clubNum', ctrl.process.createByReview);
 // 모든 후기를 보여주는 API
-router.get('/home/review/:clubNum', ctrl.process.findOneByReview);
-
+router.get('/club/review/:clubNum', ctrl.process.findOneByReview);
 module.exports = router;
