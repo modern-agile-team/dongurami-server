1615bef66dbadd4c7ede87fda0866a248e04970b
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-09-03 14:52:12 +0900

    Add(changhoon/login) : login router 생성 #46

--- app/app.js
+++ app/app.js
@@ -8,9 +8,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const signUp = require('./src/apis/sign-up');
+const login = require('./src/apis/login');
 const review = require('./src/apis/review');
 
 app.use('/api', signUp);
+app.use('/api', login);
 app.use('/api/club/review', review);
 
 module.exports = app;
