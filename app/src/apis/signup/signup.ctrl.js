d9a886a5ceefbe2834c38f6e2a5e073e5a3c5e50
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-09-03 11:01:29 +0900

    Modify(changhoon/signup) : 상태코드 수정 #28

--- app/src/apis/signup/signup.ctrl.js
+++ app/src/apis/signup/signup.ctrl.js
@@ -5,13 +5,17 @@ const Student = require('../../models/services/Student/Student');
 const process = {
   // 회원가입
   signUp: async (req, res) => {
-    const student = new Student(req.body);
-    const response = await student.signUp();
+    try {
+      const student = new Student(req.body);
+      const response = await student.signUp();
 
-    if (response.success) {
-      return res.status(201).json(response);
+      if (response.success) {
+        return res.status(201).json(response);
+      }
+      return res.status(409).json(response);
+    } catch (err) {
+      return res.status(500).json(response);
     }
-    return res.status(400).json(response);
   },
 };
 
