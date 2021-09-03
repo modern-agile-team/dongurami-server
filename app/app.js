caa405bc6b118281b00d6958ff0da12bc5c253e1
김지수 <kimjisu3817@gmail.com>
2021-09-03 22:03:18 +0900

    update

--- app/app.js
+++ app/app.js
@@ -7,9 +7,11 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const auth = require('./src/apis/root');
 const signUp = require('./src/apis/sign-up');
 const review = require('./src/apis/review');
 
+app.use('/api/auth', auth);
 app.use('/api', signUp);
 app.use('/api/club/review', review);
 
---
+++ app/src/apis/root/index.js
@@ -0,0 +1,10 @@
+// 'use strict';
+
+// const express = require('express');
+
+// const router = express.Router();
+// const ctrl = require('./root.ctrl');
+
+// router.get('/'ctrl);
+
+// modules.exports = router;
