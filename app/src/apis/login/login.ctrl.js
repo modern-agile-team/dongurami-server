cba34c188420e548440e4ca473db3ac6c041814a
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-09-05 18:31:15 +0900

    Add(changhoon/login) : controller파일의 상태코드 추가 #46

--- app/src/apis/login/login.ctrl.js
+++ app/src/apis/login/login.ctrl.js
@@ -6,7 +6,13 @@ const process = {
   login: async (req, res) => {
     const student = new Studnet(req.body);
     const response = await student.login();
-    return res.json(response);
+    if (response.success) {
+      return res.status(200).json(response);
+    }
+    if (response.isError) {
+      return res.status(500).json(response.clientMsg);
+    }
+    return res.status(400).json(response);
   },
 };
 
