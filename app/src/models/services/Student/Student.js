1e454576561bbe30cf8a64cf370e73c9996fa0e2
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-09-08 23:31:55 +0900

    Add(changhoon/reset_password) : #56
    	1. 함수명 변경 findOneByIdAndEmail -> findOneByIdOrEmail
    	2. 비밀번호 변경을 위한 비밀번호 검증  함수 생성 inspectPassword
    	3. 비밀번호 변경을 위한 함수 생성 resetPassword

--- app/src/models/services/Student/Student.js
+++ app/src/models/services/Student/Student.js
@@ -10,32 +10,6 @@ class Student {
     this.body = body;
   }
 
-  async findId() {
-    const client = this.body;
-
-    try {
-      const clientInfo = {
-        name: client.name,
-        email: client.email,
-      };
-      const student = await StudentStorage.findOneByNameAndEmail(clientInfo);
-
-      if (student) {
-        return {
-          success: true,
-          msg: '해당하는 아이디를 확인했습니다.',
-          id: student.id,
-        };
-      }
-      return { success: false, msg: '해당하는 아이디가 없습니다.' };
-    } catch (err) {
-      return Error.ctrl(
-        '알 수 없는 오류입니다. 서버개발자에게 문의하세요.',
-        err
-      );
-    }
-  }
-
   async login() {
     const client = this.body;
 
@@ -84,6 +58,60 @@ class Student {
     return inspector;
   }
 
+  async findId() {
+    const client = this.body;
+
+    try {
+      const clientInfo = {
+        name: client.name,
+        email: client.email,
+      };
+      const student = await StudentStorage.findOneByNameAndEmail(clientInfo);
+
+      if (student) {
+        return {
+          success: true,
+          msg: '해당하는 아이디를 확인했습니다.',
+          id: student.id,
+        };
+      }
+      return { success: false, msg: '해당하는 아이디가 없습니다.' };
+    } catch (err) {
+      return Error.ctrl(
+        '알 수 없는 오류입니다. 서버개발자에게 문의하세요.',
+        err
+      );
+    }
+  }
+
+  async resetPassword() {
+    const client = this.body;
+    const inspector = await this.inspectPassword();
+
+    try {
+      if (inspector.success) {
+        const saltRounds = 10;
+        const passwordSalt = bcrypt.genSaltSync(saltRounds);
+        const hash = bcrypt.hashSync(client.password, passwordSalt);
+        const clientInfo = {
+          id: inspector.student.id,
+          hash,
+          passwordSalt,
+        };
+        const student = await StudentStorage.modifypasswordSave(clientInfo);
+        if (student) {
+          return { success: true, msg: '비밀번호 변경을 성공하였습니다.' };
+        }
+      }
+      return inspector.success;
+    } catch (err) {
+      return Error.ctrl(
+        '알 수 없는 오류입니다. 서버개발자에게 문의하세요.',
+        err
+      );
+    }
+  }
+
   async inspectIdAndEmail() {
     const client = this.body;
 
@@ -92,7 +120,7 @@ class Student {
         id: client.id,
         email: client.email,
       };
-      const student = await StudentStorage.findOneByIdAndEmail(clientInfo);
+      const student = await StudentStorage.findOneByIdOrEmail(clientInfo);
 
       if (student === undefined) {
         return { saveable: true };
@@ -121,26 +149,53 @@ class Student {
     }
   }
 
+  async inspectPassword() {
+    const client = this.body; // 원비밀번호, 새로운비밀번호, 새로운 비밀번호확인
+
+    try {
+      const { id } = req.auth;
+      const student = await StudentStorage.findOneById(id);
+      const comparePassword = bcrypt.compareSync(
+        client.password,
+        student.password
+      );
+
+      if (comparePassword) {
+        if (client.newPassword === client.checkNewPassword) {
+          return { success: true, msg: '비밀번호가 일치합니다.', student };
+        }
+        return { success: false, msg: '비밀번호가 일치하지 않습니다.' };
+      }
+      return { success: false, msg: '비밀번호가 틀렸습니다.' };
+    } catch (err) {
+      return Error.ctrl(
+        '알 수 없는 오류입니다. 서버개발자에게 문의하세요.',
+        err
+      );
+    }
+  }
+
   // async isExistIdAndEmail() {
   //   const client = this.body;
 
   //   try {
-  //     const clientInfo = {
-  //       id: client.id,
-  //       email: client.email,
-  //     };
-  //     const student = await StudentStorage.findOneByIdAndEmail(clientInfo);
+  //     const { id } = req.auth;
+  //     // const clientInfo = {
+  //     //   id: client.id,
+  //     //   email: client.email,
+  //     // };
+  //     // const student = await StudentStorage.findOneByIdAndEmail(clientInfo);
 
-  //     if (student) {
-  //       if (student.id !== client.id) {
-  //         return { isExist: false, msg: '등록되지 않은 아이디입니다.' };
-  //       }
-  //       if (student.email !== client.email) {
-  //         return { isExist: false, msg: '등록되지 않은 이메일입니다.' };
-  //       }
-  //       return { isExist: true, msg: '등록된 계정입니다.', student };
-  //     }
-  //     return { isExist: false, msg: '등록되지 않은 계정입니다.' };
+  //     // if (student) {
+  //     //   if (student.id !== client.id) {
+  //     //     return { isExist: false, msg: '등록되지 않은 아이디입니다.' };
+  //     //   }
+  //     //   if (student.email !== client.email) {
+  //     //     return { isExist: false, msg: '등록되지 않은 이메일입니다.' };
+  //     //   }
+  //     //   return { isExist: true, msg: '등록된 계정입니다.', student };
+  //     // }
+  //     // return { isExist: false, msg: '등록되지 않은 계정입니다.' };
   //   } catch (err) {
   //     return Error.ctrl(
   //       '알 수 없는 오류입니다. 서버개발자에게 문의하세요.',
