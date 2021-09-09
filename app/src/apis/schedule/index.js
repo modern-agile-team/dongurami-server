cae1e8825dfd6fb198261bd06f9de65415f0148f
rugaheedori <qufslarkgml@gmail.com>
2021-09-09 17:55:26 +0900

    Modify(gahee/schedule) : 로그인 및 동아리 가입 인증 미들웨어 추가 #50

--- app/src/apis/schedule/index.js
+++ app/src/apis/schedule/index.js
@@ -4,15 +4,37 @@ const express = require('express');
 
 const router = express.Router();
 const ctrl = require('./schedule.ctrl');
+// 로그인 인증 미들웨어
+const loginAuth = require('../../middlewares/login-auth');
+// 동아리 가입 인증 미들웨어
+const clubAuth = require('../../middlewares/club-auth');
 
-router.post('/:clubNum', ctrl.process.createSchedule);
+router.post('/:clubNum', loginAuth.loginCheck, ctrl.process.createSchedule);
 
-router.get('/:clubNum', ctrl.process.findAllByClubNum);
-router.get('/:clubNum/:date', ctrl.process.findAllByDate);
+router.get(
+  '/:clubNum',
+  loginAuth.loginCheck,
+  clubAuth.clubJoinCheck,
+  ctrl.process.findAllByClubNum
+);
+router.get(
+  '/:clubNum/:date',
+  loginAuth.loginCheck,
+  clubAuth.clubJoinCheck,
+  ctrl.process.findAllByDate
+);
 
-router.put('/:clubNum/:no', ctrl.process.updateSchedule);
-router.patch('/:clubNum/:no', ctrl.process.updateOnlyImportant);
+router.put('/:clubNum/:no', loginAuth.loginCheck, ctrl.process.updateSchedule);
+router.patch(
+  '/:clubNum/:no',
+  loginAuth.loginCheck,
+  ctrl.process.updateOnlyImportant
+);
 
-router.delete('/:clubNum/:no', ctrl.process.deleteSchedule);
+router.delete(
+  '/:clubNum/:no',
+  loginAuth.loginCheck,
+  ctrl.process.deleteSchedule
+);
 
 module.exports = router;
