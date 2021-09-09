4a9770e6a6137a2047cf678731e38080706cf076
김지수 <kimjisu3817@gmail.com>
2021-09-09 16:44:31 +0900

    Refactor(jisu/admin_option): admin-option 미들웨어 추가

--- app/app.js
+++ app/app.js
@@ -13,10 +13,11 @@ const board = require('./src/apis/boards');
 const root = require('./src/apis/root');
 const review = require('./src/apis/review');
 const home = require('./src/apis/circle-home');
+const adminOption = require('./src/apis/admin-option');
 
 app.use('/api/board', board);
 app.use('/api', root);
 app.use('/api/club/review', review);
 app.use('/api/club/home', home);
-
+app.use('/api/club/admin-option', adminOption);
 module.exports = app;
