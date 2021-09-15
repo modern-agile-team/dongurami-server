303ee12b001cb7250ef22112d97fa634b0281192
rugaheedori <qufslarkgml@gmail.com>
2021-09-15 15:03:38 +0900

    Delete(gahee/application-form) : 불필요한 retrun값 삭제#75

--- app/src/apis/application-form/application.ctrl.js
+++ app/src/apis/application-form/application.ctrl.js
@@ -20,45 +20,30 @@ const process = {
     const application = new Application(req);
     const response = await application.createQuestion();
 
-    if (response.success) {
-      return res.status(201).json(response);
-    }
     if (response.isError) {
       return res.status(500).json({ success: false, msg: response.clientMsg });
     }
-    return res
-      .status(400)
-      .json({ success: false, msg: '서버 개발자에게 문의해주세요.' });
+    return res.status(201).json(response);
   },
 
   updateQuestion: async (req, res) => {
     const application = new Application(req);
     const response = await application.updateQuestion();
 
-    if (response.success) {
-      return res.status(200).json(response);
-    }
     if (response.isError) {
       return res.status(500).json({ success: false, msg: response.clientMsg });
     }
-    return res
-      .status(400)
-      .json({ success: false, msg: '서버 개발자에게 문의해주세요.' });
+    return res.status(200).json(response);
   },
 
   deleteQuestion: async (req, res) => {
     const application = new Application(req);
     const response = await application.deleteQuestion();
 
-    if (response.success) {
-      return res.status(200).json(response);
-    }
     if (response.isError) {
       return res.status(500).json({ success: false, msg: response.clientMsg });
     }
-    return res
-      .status(400)
-      .json({ success: false, msg: '서버 개발자에게 문의해주세요.' });
+    return res.status(200).json(response);
   },
 };
 
