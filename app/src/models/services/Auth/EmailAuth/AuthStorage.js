487fa68fc3a91951c3ce24da7a2cc7de875987d7
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-09-17 11:54:38 +0900

    Add(changhoon/notification_password) : .env파일에 redis의 host와 port 추가 #55

--- app/src/models/services/Auth/EmailAuth/AuthStorage.js
+++ app/src/models/services/Auth/EmailAuth/AuthStorage.js
@@ -3,8 +3,8 @@
 const db = require('redis');
 
 const redis = db.createClient({
-  host: '127.0.0.1',
-  port: 6379,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
 });
 
 redis.on('error', (error) => {
