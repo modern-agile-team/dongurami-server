d64f3f6436038fae4b41caaa804eac50674ecd31
rugaheedori <qufslarkgml@gmail.com>
2021-09-28 13:38:19 +0900

    Modify(gahee/club-board) : 로그인 확인 middleware추가, index변경 #116

--- app/src/apis/club-board/index.js
+++ app/src/apis/club-board/index.js
@@ -1,41 +1,68 @@
 'use strict';
 
 const express = require('express');
-const ctrl = require('../boards/board.ctrl');
+const boardCtrl = require('../boards/board.ctrl');
 const commentCtrl = require('../boards/comment.ctrl');
+const loginCheck = require('../../middlewares/login-auth');
 
 const router = express.Router();
 
 router.get(
   '/:category/:clubNum/:sort/:order',
-  ctrl.process.findAllByCategoryNum
+  loginCheck.loginCheck,
+  boardCtrl.process.findAllByCategoryNum
 );
-router.get('/:category/:clubNum/:boardNum', ctrl.process.findOneByBoardNum);
+router.get(
+  '/:category/:clubNum/:boardNum',
+  loginCheck.loginCheck,
+  boardCtrl.process.findOneByBoardNum
+);
 
-router.post('/:category/:clubNum', ctrl.process.createBoardNum);
-router.post('/:category/:boardNum', commentCtrl.process.createCommentNum);
 router.post(
-  '/:category/:boardNum/:cmtNum',
+  '/:category/:clubNum',
+  loginCheck.loginCheck,
+  boardCtrl.process.createBoardNum
+);
+router.post(
+  '/:category/:clubNum/:boardNum',
+  loginCheck.loginCheck,
+  commentCtrl.process.createCommentNum
+);
+router.post(
+  '/:category/:clubNum/:boardNum/:cmtNum',
+  loginCheck.loginCheck,
   commentCtrl.process.createReplyCommentNum
 );
 
-router.put('/:category/:clubNum', ctrl.process.updateOneByBoardNum);
+router.put(
+  '/:category/:clubNum',
+  loginCheck.loginCheck,
+  boardCtrl.process.updateOneByBoardNum
+);
 router.put(
   '/:category/:boardNum/:cmtNum',
+  loginCheck.loginCheck,
   commentCtrl.process.updateByCommentNum
 );
 router.put(
   '/:category/:boardNum/:cmtNum/:replyCmtNum',
+  loginCheck.loginCheck,
   commentCtrl.process.updateByReplyCommentNum
 );
 
-router.delete('/:category/:clubNum', ctrl.process.deleteOneByBoardNum);
+router.delete(
+  '/:category/:clubNum/:boardNum',
+  loginCheck.loginCheck,
+  boardCtrl.process.deleteOneByBoardNum
+);
 router.delete(
   '/:category/:boardNum/:cmtNum',
+  loginCheck.loginCheck,
   commentCtrl.process.deleteAllByGroupNum
 );
 router.delete(
   '/:category/:boardNum/:cmtNum/:replyCmtNum',
+  loginCheck.loginCheck,
   commentCtrl.process.deleteOneReplyCommentNum
 );
 
