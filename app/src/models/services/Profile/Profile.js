fa01b47caee2403b37fc9c3da33eb1086d6484ff
lsh <rrgks@naver.com>
2021-10-08 14:10:40 +0900

    Modify(seokho/profile): 프로필 조회시 본인 프로필이 아닐 때 응답바디 제한 #127

--- app/src/models/services/Profile/Profile.js
+++ app/src/models/services/Profile/Profile.js
@@ -20,6 +20,12 @@ class Profile {
       if (profile === undefined) {
         return { success: false, msg: '존재하지 않는 회원입니다.' };
       }
+      if (profile.id !== user.id) {
+        delete profile.email;
+        delete profile.phoneNumber;
+        delete profile.grade;
+        delete profile.gender;
+      }
 
       const clubs = await ProfileStorage.findAllClubByStudentId(id);
 
