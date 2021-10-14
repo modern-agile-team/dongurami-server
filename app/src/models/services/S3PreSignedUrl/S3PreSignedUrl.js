6f0204af07ff8727d04e53c7152cfb1285a0af74
lsh <rrgks@naver.com>
2021-10-14 17:26:01 +0900

    Modify(seokho/s3-pre-signed-url: 파일 이름 중복 제거 및 response에 cloudfront 주소 추가 #183

--- app/src/models/services/S3PreSignedUrl/S3PreSignedUrl.js
+++ app/src/models/services/S3PreSignedUrl/S3PreSignedUrl.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const crypto = require('crypto');
 const Error = require('../../utils/Error');
 
 class S3 {
@@ -9,13 +10,18 @@ class S3 {
   }
 
   async createPutUrl() {
-    const { img } = this.body;
+    let { img } = this.body;
 
     if (img === undefined) {
       return { success: false, msg: '객체 이름을 적어주세요' };
     }
 
     try {
+      const randomString = crypto.randomBytes(5).toString('hex');
+
+      img = `${randomString}_${img}`;
+      const readObjectUrl = process.env.CLOUDFRONT_URL + img;
+
       const s3 = new AWS.S3({
         accessKeyId: process.env.S3_ACCESS_KEY,
         secretAccessKey: process.env.S3_SECRET_KEY,
@@ -28,9 +34,13 @@ class S3 {
         Key: img,
       });
 
-      return { success: true, msg: 'url 생성 성공', preSignedPutUrl };
+      return {
+        success: true,
+        msg: 'url 생성 성공',
+        preSignedPutUrl,
+        readObjectUrl,
+      };
     } catch (err) {
-      console.log(err);
       return Error.ctrl('서버 에러입니다. 서버 개발자에게 얘기해주세요', err);
     }
   }
