a9e149d13cafafd773718b8ac824d91a171665f8
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-10-18 11:17:40 +0900

    Add(changhoon/logger) : #187
    	1. 모듈 설치 (winston / winston-daily-rotate-file)
    	2. 로그 양식 / 출력 양식 설정
    	3. 옵션설정
    	4. 개발환경 / 배포환경 분리
    	5. 파일저장 / 콘솔창 분리

---
+++ app/src/config/logger.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const { transports, createLogger, format } = require('winston');
+
+const { combine, timestamp, printf, colorize, simple } = format;
+require('winston-daily-rotate-file');
+
+const printFormat = printf(
+  (info) => `${info.timestamp} [ ${info.level} ] ${info.message}`
+);
+
+const printLogFormat = {
+  file: combine(
+    timestamp({
+      format: 'YYYY-MM-DD HH:mm:dd',
+    }),
+    printFormat
+  ),
+
+  console: combine(colorize(), simple()),
+};
+
+const opts = {
+  file: new transports.DailyRotateFile({
+    filename: '%DATE%.log',
+    dirname: './src/config/logs',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    maxSize: '20m',
+    maxFiles: '7d',
+    format: printLogFormat.file,
+  }),
+
+  console: new transports.Console({
+    level: 'info',
+    format: printLogFormat.console,
+  }),
+};
+
+const logger = createLogger({
+  transports: [opts.file],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(opts.console);
+}
+
+logger.stream = {
+  write: (message) => logger.info(message),
+};
+
+module.exports = logger;
