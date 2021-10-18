d3966a78d7bfb58ba89ecdafc3eab20e72289b9e
김지수 <kimjisu3817@gmail.com>
2021-10-18 16:47:02 +0900

    Refactory(jisu/review): review.ctrl put, delete 요청 상태코드 200으로 수정 #208

--- app/src/apis/review/review.ctrl.js
+++ app/src/apis/review/review.ctrl.js
@@ -4,22 +4,6 @@ const Review = require('../../models/services/Review/Review');
 const logger = require('../../config/logger');
 
 const process = {
-  createByReview: async (req, res) => {
-    const review = new Review(req);
-    const response = await review.createByReivew();
-
-    if (response.success) {
-      logger.info(`POST /clubNum 201: ${response.msg}`);
-      return res.status(201).json(response);
-    }
-    if (response.isError) {
-      logger.error(`POST /clubNum 500: \n${response.msg}`);
-      return res.status(500).json(response.clientMsg);
-    }
-    logger.error(`POST /clubNum 500: \n${response.msg}`);
-    return res.status(400).json(response);
-  },
-
   findOneByClubNum: async (req, res) => {
     const review = new Review(req);
     const response = await review.findOneByClubNum();
@@ -33,6 +17,22 @@ const process = {
       return res.status(500).json(response.clientMsg);
     }
     logger.error(`GET /clubNum 400: ${response.msg}`);
+    return res.status(400).json(response);
+  },
+
+  createByReview: async (req, res) => {
+    const review = new Review(req);
+    const response = await review.createByReivew();
+
+    if (response.success) {
+      logger.info(`POST /clubNum 201: ${response.msg}`);
+      return res.status(201).json(response);
+    }
+    if (response.isError) {
+      logger.error(`POST /clubNum 500: \n${response.msg}`);
+      return res.status(500).json(response.clientMsg);
+    }
+    logger.error(`POST /clubNum 500: \n${response.msg}`);
     return res.status(400).json(response);
   },
 
