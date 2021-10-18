571acbe550ed4b3cd9f8c3b27c089314745598b6
rugaheedori <qufslarkgml@gmail.com>
2021-10-18 16:12:32 +0900

    Delete(gahee/develop) : 모든 기능에서 사용하지 않는 file_id column 삭제

--- app/src/models/services/Board/Comment/CommentStorage.js
+++ app/src/models/services/Board/Comment/CommentStorage.js
@@ -54,7 +54,7 @@ class CommentStorage {
     try {
       conn = await mariadb.getConnection();
 
-      const query = `SELECT cmt.student_id AS studentId, st.name AS studentName, cmt.no, cmt.description, cmt.depth, cmt.group_no AS groupNo, cmt.reply_flag AS replyFlag, cmt.in_date AS inDate, cmt.modify_date AS modifyDate, st.profile_image_url AS profileImageUrl, st.file_id As profileImageName
+      const query = `SELECT cmt.student_id AS studentId, st.name AS studentName, cmt.no, cmt.description, cmt.depth, cmt.group_no AS groupNo, cmt.reply_flag AS replyFlag, cmt.in_date AS inDate, cmt.modify_date AS modifyDate, st.profile_image_url AS profileImageUrl
       FROM comments AS cmt
       JOIN students AS st
       ON cmt.student_id = st.id
