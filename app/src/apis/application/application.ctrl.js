27eb4f36daf7f3b3dc01a1182659bd912cc7040d
rugaheedori <qufslarkgml@gmail.com>
2021-10-19 17:08:24 +0900

    Refactor(gahee/application-form) : logger 추가 #226

--- app/src/apis/application/application.ctrl.js
+++ app/src/apis/application/application.ctrl.js
@@ -9,14 +9,16 @@ const process = {
     const response = await application.findAllByClubNum();
 
     if (response.success) {
-      logger.info(`GET /clubNum 200: ${response.msg}`);
+      logger.info(`GET /api/club/application/clubNum 200: ${response.msg}`);
       return res.status(200).json(response);
     }
     if (response.isError) {
-      logger.error(`GET /clubNum 500: \n${response.errMsg}`);
+      logger.error(
+        `GET /api/club/application/clubNum 500: \n${response.errMsg}`
+      );
       return res.status(500).json({ success: false, msg: response.clientMsg });
     }
-    logger.error(`GET /clubNum 404: ${response.msg}`);
+    logger.error(`GET /api/club/application/clubNum 404: ${response.msg}`);
     return res.status(404).json(response); // 동아리가 존재 하지 않을 시
   },
 
@@ -25,14 +27,16 @@ const process = {
     const response = await application.createQuestion();
 
     if (response.success) {
-      logger.info(`POST /clubNum 201: ${response.msg}`);
+      logger.info(`POST /api/club/application/clubNum 201: ${response.msg}`);
       return res.status(201).json(response);
     }
     if (response.isError) {
-      logger.error(`POST /clubNum 500: \n${response.errMsg}`);
+      logger.error(
+        `POST /api/club/application/clubNum 500: \n${response.errMsg}`
+      );
       return res.status(500).json({ success: false, msg: response.clientMsg });
     }
-    logger.error(`POST /clubNum 400: ${response.msg}`);
+    logger.error(`POST /api/club/application/clubNum 400: ${response.msg}`);
     return res.status(400).json(response);
   },
 
@@ -41,14 +45,16 @@ const process = {
     const response = await application.updateQuestion();
 
     if (response.success) {
-      logger.info(`PUT /clubNum 200: ${response.msg}`);
+      logger.info(`PUT /api/club/application/clubNum/no 200: ${response.msg}`);
       return res.status(200).json(response);
     }
     if (response.isError) {
-      logger.error(`PUT /clubNum 500: \n${response.errMsg}`);
+      logger.error(
+        `PUT /api/club/application/clubNum/no 500: \n${response.errMsg}`
+      );
       return res.status(500).json({ success: false, msg: response.clientMsg });
     }
-    logger.error(`PUT /clubNum 400: ${response.msg}`);
+    logger.error(`PUT /api/club/application/clubNum/no 400: ${response.msg}`);
     return res.status(400).json(response);
   },
 
@@ -57,14 +63,20 @@ const process = {
     const response = await application.deleteQuestion();
 
     if (response.success) {
-      logger.info(`DELETE /clubNum 200: ${response.msg}`);
+      logger.info(
+        `DELETE /api/club/application/clubNum/no 200: ${response.msg}`
+      );
       return res.status(200).json(response);
     }
     if (response.isError) {
-      logger.error(`DELETE /clubNum 500: \n${response.errMsg}`);
+      logger.error(
+        `DELETE /api/club/application/clubNum/no 500: \n${response.errMsg}`
+      );
       return res.status(500).json({ success: false, msg: response.clientMsg });
     }
-    logger.error(`DELETE /clubNum 400: ${response.msg}`);
+    logger.error(
+      `DELETE /api/club/application/clubNum/no 400: ${response.msg}`
+    );
     return res.status(400).json(response);
   },
 
@@ -73,14 +85,20 @@ const process = {
     const response = await application.createAnswer();
 
     if (response.success) {
-      logger.info(`POST /clubNum 201: ${response.msg}`);
+      logger.info(
+        `POST /api/club/application/clubNum/answer 201: ${response.msg}`
+      );
       return res.status(201).json(response);
     }
     if (response.isError) {
-      logger.error(`POST /clubNum 500: \n${response.errMsg}`);
+      logger.error(
+        `POST /api/club/application/clubNum/answer 500: \n${response.errMsg}`
+      );
       return res.status(500).json({ success: false, msg: response.clientMsg });
     }
-    logger.error(`POST /clubNum 400: ${response.msg}`);
+    logger.error(
+      `POST /api/club/application/clubNum/answer 400: ${response.msg}`
+    );
     return res.status(400).json(response);
   },
 };
