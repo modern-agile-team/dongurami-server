7919778586b7e1ae7187394080b8d8831eb50859
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-10-20 15:41:41 +0900

    Add(changhoon/promotionSearch) : 홍보게시판 검색 컨트롤러 설정 & 로그 설정 #236

--- app/src/apis/search/search.ctrl.js
+++ app/src/apis/search/search.ctrl.js
@@ -19,6 +19,24 @@ const process = {
     logger.error(`GET /api/search/category 400: ${response.msg}`);
     return res.status(400).json(response);
   },
+
+  promotionSearch: async (req, res) => {
+    const board = new Board(req);
+    const response = await board.promotionSearch();
+
+    if (response.success) {
+      logger.info(`GET /api/search/promotion/category 200: ${response.msg}`);
+      return res.status(200).json(response);
+    }
+    if (response.isError) {
+      logger.error(
+        `GET /api/search/promotion/category 500:\n${response.errMsg}`
+      );
+      return res.status(500).json(response.clientMsg);
+    }
+    logger.error(`GET /api/search/promotion/category 400: ${response.msg}`);
+    return res.status(400).json(response);
+  },
 };
 
 module.exports = { process };
