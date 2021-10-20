f3e426a60787e2ea94f7d4a7f291872e50393df4
rugaheedori <qufslarkgml@gmail.com>
2021-10-20 16:35:15 +0900

    Refactor(gahee/circle-home) : logger 수정 #231

--- app/src/apis/circle-home/home.ctrl.js
+++ app/src/apis/circle-home/home.ctrl.js
@@ -40,12 +40,16 @@ const process = {
     const home = new Home(req);
     const response = await home.updateClubLogo();
 
+    if (response.success) {
+      logger.info(`PUT /api/club/home/clubNum 200 : ${response.msg}`);
+      return res.status(200).json(response);
+    }
     if (response.isError) {
       logger.error(`PUT /api/club/home/clubNum 500 : \n${response.errMsg}`);
       return res.status(500).json({ success: false, msg: response.clientMsg });
     }
-    logger.info(`PUT /api/club/home/clubNum 200 : \n${response.msg}`);
-    return res.status(200).json(response);
+    logger.error(`PUT /api/club/home/clubNum 403 : ${response.msg}`);
+    return res.status(403).json(response);
   },
 };
 
