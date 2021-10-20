c2fb9d83d927a1b95b8673d1c76492e61b7b7c5d
김지수 <kimjisu3817@gmail.com>
2021-10-20 15:35:01 +0900

    Refactor(jisu/auth): 로그인 인증 로그관리 수정 #231

--- app/src/middlewares/club-auth.js
+++ app/src/middlewares/club-auth.js
@@ -1,10 +1,15 @@
 'use strict';
 
+const logger = require('../config/logger');
+
 const clubJoinCheck = (req, res, next) => {
   const paramsClubNum = Number(req.params.clubNum);
   const clubs = req.auth.clubNum;
 
   if (!clubs.includes(paramsClubNum) || clubs.length === 0) {
+    logger.error(
+      `GET /api/sign-up 401: JWT 토큰이 존재하지 않습니다. 로그인 후 이용해주세요.`
+    );
     return res
       .status(403)
       .json({ success: false, msg: '해당 동아리에 가입하지 않았습니다.' });
