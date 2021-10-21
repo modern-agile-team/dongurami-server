97eb4947f51cc6b1254fd13b8fb3d561d5e4d31b
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-10-21 12:00:04 +0900

    Refactor(changhoon/search) : search 로그관리와 코드 리팩터링 #245

--- app/src/apis/search/search.ctrl.js
+++ app/src/apis/search/search.ctrl.js
@@ -6,20 +6,18 @@ const logger = require('../../config/logger');
 const process = {
   search: async (req, res) => {
     const board = new Board(req);
-    const { params } = req;
+    const { category } = req.params;
     const response = await board.search();
 
     if (response.success) {
-      logger.info(`GET /api/search/${params.category} 200: ${response.msg}`);
+      logger.info(`GET /api/search/${category} 200: ${response.msg}`);
       return res.status(200).json(response);
     }
     if (response.isError) {
-      logger.error(
-        `GET /api/search/${params.category} 500: \n${response.errMsg}`
-      );
+      logger.error(`GET /api/search/${category} 500: \n${response.errMsg}`);
       return res.status(500).json(response.clientMsg);
     }
-    logger.error(`GET /api/search/${params.category} 400: ${response.msg}`);
+    logger.error(`GET /api/search/${category} 400: ${response.msg}`);
     return res.status(400).json(response);
   },
 
