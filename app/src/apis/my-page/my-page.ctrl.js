20b4b4c32ce0811ef157af5c83effe71da1cabae
rugaheedori <qufslarkgml@gmail.com>
2021-10-21 10:35:02 +0900

    Refactor(gahee/my-page) : logger 수정 #245

--- app/src/apis/my-page/my-page.ctrl.js
+++ app/src/apis/my-page/my-page.ctrl.js
@@ -6,106 +6,115 @@ const logger = require('../../config/logger');
 const process = {
   findAllScrapsByClubNum: async (req, res) => {
     const myPage = new MyPage(req);
+    const { params } = req;
     const response = await myPage.findAllScrapsByClubNum();
 
     if (response.success) {
-      logger.info(`GET /api/my-page/id/personal/clubNum 200: ${response.msg}`);
+      logger.info(
+        `GET /api/my-page/${params.id}/personal/${params.clubNum} 200: ${response.msg}`
+      );
       return res.status(200).json(response);
     }
     if (response.isError) {
       logger.error(
-        `GET /api/my-page/id/personal/clubNum 500: \n${response.errMsg}`
+        `GET /api/my-page/${params.id}/personal/${params.clubNum}m 500: \n${response.errMsg}`
       );
       return res.status(500).json(response.clientMsg);
     }
-    logger.error(`GET /api/my-page/id/personal/clubNum 404: ${response.msg}`);
+    logger.error(
+      `GET /api/my-page/${params.id}/personal/${params.clubNum} 404: ${response.msg}`
+    );
     return res.status(404).json(response);
   },
 
   findOneScrap: async (req, res) => {
     const myPage = new MyPage(req);
+    const { params } = req;
     const response = await myPage.findOneScrap();
 
     if (response.success) {
       logger.info(
-        `GET /api/my-page/id/personal/scrap/clubNum/scrpaNum 200: ${response.msg}`
+        `GET /api/my-page/${params.id}/personal/scrap/${params.clubNum}/${params.scrapNum} 200: ${response.msg}`
       );
       return res.status(200).json(response);
     }
     if (response.isError) {
       logger.error(
-        `GET /api/my-page/id/personal/scrap/clubNum/scrpaNum 500: \n${response.errMsg}`
+        `GET /api/my-page/${params.id}/personal/scrap/${params.clubNum}/${params.scrapNum} 500: \n${response.errMsg}`
       );
       return res.status(500).json(response.clientMsg);
     }
     logger.error(
-      `GET /api/my-page/id/personal/scrap/clubNum/scrpaNum 404: ${response.msg}`
+      `GET /api/my-page/${params.id}/personal/scrap/${params.clubNum}/${params.scrapNum} 404: ${response.msg}`
     );
     return res.status(404).json(response);
   },
 
   createScrapNum: async (req, res) => {
     const board = new MyPage(req);
+    const { params } = req;
     const response = await board.createScrapNum();
 
     if (response.success) {
       logger.info(
-        `POST /api/club/board/category/personal/scrap/clubNum/boardNum 201: ${response.msg}`
+        `POST /api/club/board/${params.category}/personal/scrap/${params.clubNum}/${params.boardNum} 201: ${response.msg}`
       );
       return res.status(201).json(response);
     }
     if (response.isError) {
       logger.error(
-        `POST /api/club/board/category/personal/scrap/clubNum/boardNum 500: \n${response.errMsg}`
+        `POST /api/club/board/${params.category}/personal/scrap/${params.clubNum}/${params.boardNum} 500: \n${response.errMsg}`
       );
       return res.status(500).json(response.clientMsg);
     }
     logger.error(
-      `POST /api/club/board/category/personal/scrap/clubNum/boardNum 400: ${response.msg}`
+      `POST /api/club/board/${params.category}/personal/scrap/${params.clubNum}/${params.boardNum} 400: ${response.msg}`
     );
     return res.status(400).json(response);
   },
 
   updateOneByScrapNum: async (req, res) => {
     const myPage = new MyPage(req);
+    const { params } = req;
     const response = await myPage.updateOneByScrapNum();
 
     if (response.success) {
       logger.info(
-        `PUT /api/my-page/id/personal/scrap/clubNum/scrapNum 200: ${response.msg}`
+        `PUT /api/my-page/${params.id}/personal/scrap/${params.clubNum}/${params.scrapNum} 200: ${response.msg}`
       );
       return res.status(200).json(response);
     }
     if (response.isError) {
       logger.error(
-        `PUT /api/my-page/id/personal/scrap/clubNum/scrapNum 500: \n${response.errMsg}`
+        `PUT /api/my-page/${params.id}/personal/scrap/${params.clubNum}/${params.scrapNum} 500: \n${response.errMsg}`
       );
       return res.status(500).json(response.clientMsg);
     }
     logger.error(
-      `PUT /api/my-page/id/personal/scrap/clubNum/scrapNum 400: ${response.msg}`
+      `PUT /api/my-page/${params.id}/personal/scrap/${params.clubNum}/${params.scrapNum} 400: ${response.msg}`
     );
     return res.status(400).json(response);
   },
 
   deleteOneByScrapNum: async (req, res) => {
     const myPage = new MyPage(req);
+    const { params } = req;
     const response = await myPage.deleteOneByScrapNum();
 
     if (response.success) {
       logger.info(
-        `DELETE /api/my-page/id/personal/scrap/clubNum/scrapNum 200: ${response.msg}`
+        `DELETE /api/my-page/${params.id}/personal/scrap/${params.clubNum}/${params.scrapNum} 200: ${response.msg}`
       );
       return res.status(200).json(response);
     }
     if (response.isError) {
       logger.error(
-        `DELETE /api/my-page/id/personal/scrap/clubNum/scrapNum 500:\n${response.errMsg}`
+        `DELETE /api/my-page/${params.id}/personal/scrap/${params.clubNum}/${params.scrapNum} 500:\n${response.errMsg}`
       );
       return res.status(500).json(response);
     }
     logger.error(
-      `DELETE /api/my-page/id/personal/scrap/clubNum/scrapNum 400: ${response.msg}`
+      `DELETE /api/my-page/${params.id}/personal/scrap/${params.clubNum}/${params.scrapNum} 400: ${response.msg}`
     );
     return res.status(400).json(response);
   },
