fe317b76a5c50ce145fb4d9b4171d4774f091e15
lsh <rrgks@naver.com>
2021-10-23 23:12:58 +0900

    Refactor(seokho/boards): 게시글 수정 삭제 상태코드 400 => 404 수정 #257

--- app/src/apis/boards/board.ctrl.js
+++ app/src/apis/boards/board.ctrl.js
@@ -147,8 +147,8 @@ const process = {
       );
       return res.status(500).json(response.clientMsg);
     }
-    logger.error(`PUT /api/board/${category}/${boardNum} 400: ${response.msg}`);
-    return res.status(400).json(response);
+    logger.error(`PUT /api/board/${category}/${boardNum} 404: ${response.msg}`);
+    return res.status(404).json(response);
   },
 
   deleteOneByBoardNum: async (req, res) => {
@@ -170,9 +170,9 @@ const process = {
       return res.status(500).json(response.clientMsg);
     }
     logger.error(
-      `DELETE /api/board/${category}/${boardNum} 400: ${response.msg}`
+      `DELETE /api/board/${category}/${boardNum} 404: ${response.msg}`
     );
-    return res.status(400).json(response);
+    return res.status(404).json(response);
   },
 };
 
