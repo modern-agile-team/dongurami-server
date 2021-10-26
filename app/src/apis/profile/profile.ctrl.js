34febd896f4b1fec308d25755f9cafa1eb6b8ad4
lsh <rrgks@naver.com>
2021-10-26 13:42:54 +0900

    Refactor(seokho/profile): 500번 에러 로거 \n 추가 #270

--- app/src/apis/profile/profile.ctrl.js
+++ app/src/apis/profile/profile.ctrl.js
@@ -14,7 +14,7 @@ const process = {
       return res.status(200).json(response);
     }
     if (response.isError) {
-      logger.error(`GET /api/profile/${studentId} 500: ${response.errMsg}`);
+      logger.error(`GET /api/profile/${studentId} 500: \n${response.errMsg}`);
       return res.status(500).json(response.clientMsg);
     }
     logger.error(`GET /api/profile/${studentId} 400: ${response.msg}`);
@@ -31,7 +31,7 @@ const process = {
       return res.status(200).json(response);
     }
     if (response.isError) {
-      logger.error(`PUT /api/profile/${studentId} 500: ${response.errMsg}`);
+      logger.error(`PUT /api/profile/${studentId} 500: \n${response.errMsg}`);
       return res.status(500).json(response.clientMsg);
     }
     logger.error(`PUT /api/profile/${studentId} 400: ${response.msg}`);
