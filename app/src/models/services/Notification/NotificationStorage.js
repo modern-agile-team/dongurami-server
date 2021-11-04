d38d5f63e58df95140774e9e8ebfbe10a00ff4ff
김지수 <kimjisu@gimjisuui-MacBookPro.local>
2021-11-04 17:21:54 +0900

    Refactor(jisu/notifcation): 알림 API 쿼리문 수정 #196

--- app/src/models/services/Notification/NotificationStorage.js
+++ app/src/models/services/Notification/NotificationStorage.js
@@ -56,7 +56,7 @@ class NotificationStorage {
     try {
       conn = await mariadb.getConnection();
 
-      const query = `INSERT INTO notifications (sender_id, recipient_id, url, notification_category_no, title, content) VALUES (?, ?, ?, ?, ?, ?);`;
+      const query = `INSERT INTO notifications (sender, recipient, url, notification_category_no, title, content) VALUES (?, ?, ?, ?, ?, ?);`;
 
       await conn.query(query, [
         notificationInfo.senderName,
