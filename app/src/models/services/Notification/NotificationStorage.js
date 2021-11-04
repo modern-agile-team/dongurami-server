1ea2ef4b58a59889f7e39d22f22c1892fe7e8c04
김지수 <kimjisu@gimjisuui-MacBookPro.local>
2021-11-04 21:17:39 +0900

    Refactor(jisu/notification): 알림 조회 쿼리 수정 #196

--- app/src/models/services/Notification/NotificationStorage.js
+++ app/src/models/services/Notification/NotificationStorage.js
@@ -9,14 +9,14 @@ class NotificationStorage {
     try {
       conn = await mariadb.getConnection();
 
-      const query = `SELECT no.no AS notificationNum, no.sender AS sender, 
-        no.url, no.notification_category_no AS notificationCategoryNum, 
-        no.in_date AS inDate FROM notifications AS no 
-        WHERE no.recipient = (SELECT id FROM students WHERE id = ?) AND no.reading_flag = 0
+      const query = `SELECT no, no.sender, no.url, no.in_date AS inDate, no.notification_category_no AS notiCategoryNum
+        FROM notifications AS no 
+        WHERE no.recipient = (SELECT name FROM  students WHERE id = ?) AND no.reading_flag = 0
         ORDER BY inDate DESC;`;
 
-      const notifications = await conn.query(query, studentId);
+      const notifications = await conn.query(query, [studentId]);
 
+      console.log(notifications);
       return { success: true, notifications };
     } catch (err) {
       throw err;
