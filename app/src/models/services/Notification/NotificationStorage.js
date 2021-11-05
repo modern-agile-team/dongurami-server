8946ac9cd1b0900c43ba4ac00ebd6eda88231c9f
김지수 <kimjisu@gimjisuui-MacBookPro.local>
2021-11-05 12:18:15 +0900

    Conflict(jisu/notification): 충돌 해결#196

--- app/src/models/services/Notification/NotificationStorage.js
+++ app/src/models/services/Notification/NotificationStorage.js
@@ -11,7 +11,7 @@ class NotificationStorage {
 
       const query = `SELECT no, no.sender, no.url, no.notification_category_no AS notiCategoryNum,
         no.in_date AS inDate FROM notifications AS no 
-        WHERE no.recipient = (SELECT name FROM  students WHERE id = ?) AND no.reading_flag = 0
+        WHERE no.recipient = (SELECT name FROM students WHERE id = ?) AND no.reading_flag = 0
         ORDER BY inDate DESC;`;
 
       const notifications = await conn.query(query, [studentId]);
