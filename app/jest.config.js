9246dc3006e3b695d114a706bdd1b07ab9e81ed5
김지수 <kimjisu@gimjisuui-MacBookPro.local>
2021-11-10 16:40:44 +0900

    Refactor(jisu/jest): jest.comfig 파일 수정#330

--- app/jest.config.js
+++ app/jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   bail: true,
   verbose: true,
   testRegex: '(/tests/.*[.](test|spec))\\.(ts|tsx|js)$',
-  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.js'],
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json'],
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   coveragePathIgnorePatterns: ['/node_modules/'],
