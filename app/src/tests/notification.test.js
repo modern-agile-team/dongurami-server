c4434b86cb33aaf05ec215bb7c73274157e3988b
김지수 <kimjisu@gimjisuui-MacBookPro.local>
2021-11-12 15:10:03 +0900

    Add(jisu/jest): 동아리 알림 생성 수정 알림 테스트 생성 #330

--- app/src/tests/notification.test.js
+++ app/src/tests/notification.test.js
@@ -5,18 +5,44 @@ const server = request(app);
 
 const comment = {
   description: '댓글 테스트!! 이 댓글은 지우지 마세요 -김지수-',
-  notiCategoryNum: 0,
   url: 'test/url',
+  notiCategoryNum: 0,
 };
 
 const reply = {
   description: '답글 테스트!! 이 댓글은 지우지 마세요 -김지수-',
-  notiCategoryNum: 1,
   url: 'test/url',
+  notiCategoryNum: 1,
 };
 
-const clubAdmin = {
+const apply = {
   applicant: '000000011',
+  url: 'test/url',
+  notiCategoryNum: 2,
+};
+
+const reject = {
+  applicant: '000000011',
+  url: 'test/url',
+  notiCategoryNum: 3,
+};
+
+const createSchedule = {
+  title: '알림생성 테스트!! 이 일정은 지우지 마세요 -김지수-',
+  colorCode: '#123456',
+  startDate: '2021-11-12',
+  endDate: '2021-11-12',
+  url: 'schedule/notification/test',
+  notiCategoryNum: 4,
+};
+
+const modifySchedule = {
+  title: '알림수정 테스트!! 이 일정은 지우지 마세요 -김지수-',
+  colorCode: '#123456',
+  startDate: '2021-11-12',
+  endDate: '2021-11-12',
+  url: 'schedule/notification/test',
+  notiCategoryNum: 5,
 };
 
 describe('알림 API 테스트', () => {
@@ -61,7 +87,7 @@ describe('알림 API 테스트', () => {
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHViTnVtIjpbMl0sImlkIjoiMDAwMDAwMDA1IiwibmFtZSI6IuuLpOyEryIsInByb2ZpbGVQYXRoIjpudWxsLCJpc0FkbWluIjowLCJpYXQiOjE2MzY2MzM4MjMsImV4cCI6MTYzNjcyMDIyMywiaXNzIjoid29vYWhhbiBhZ2lsZSJ9.nWkuNJp0Odo-y3on9VV18XDoTEgxvnuNOKyskPEM4Fg'
         )
         .set('Content-Type', 'application/json')
-        .send(clubAdmin);
+        .send(apply);
       expect(res.statusCode).toEqual(201);
     } catch (err) {
       console.log(err);
@@ -77,7 +103,39 @@ describe('알림 API 테스트', () => {
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHViTnVtIjpbMl0sImlkIjoiMDAwMDAwMDA1IiwibmFtZSI6IuuLpOyEryIsInByb2ZpbGVQYXRoIjpudWxsLCJpc0FkbWluIjowLCJpYXQiOjE2MzY2MzM4MjMsImV4cCI6MTYzNjcyMDIyMywiaXNzIjoid29vYWhhbiBhZ2lsZSJ9.nWkuNJp0Odo-y3on9VV18XDoTEgxvnuNOKyskPEM4Fg'
         )
         .set('Content-Type', 'application/json')
-        .send(clubAdmin);
+        .send(reject);
+      expect(res.statusCode).toEqual(200);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  it('POST 동아리 일정 알림생성 시 201 반환', async () => {
+    try {
+      const res = await server
+        .post('/api/club/schedule/2')
+        .set(
+          'x-auth-token',
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHViTnVtIjpbMl0sImlkIjoiMDAwMDAwMDA1IiwibmFtZSI6IuuLpOyEryIsInByb2ZpbGVQYXRoIjpudWxsLCJpc0FkbWluIjowLCJpYXQiOjE2MzY2MzM4MjMsImV4cCI6MTYzNjcyMDIyMywiaXNzIjoid29vYWhhbiBhZ2lsZSJ9.nWkuNJp0Odo-y3on9VV18XDoTEgxvnuNOKyskPEM4Fg'
+        )
+        .set('Content-Type', 'application/json')
+        .send(modifySchedule);
+      expect(res.statusCode).toEqual(201);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  it('POST 동아리 일정 수정 알림생성 시 200 반환', async () => {
+    try {
+      const res = await server
+        .put('/api/club/schedule/2/1')
+        .set(
+          'x-auth-token',
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHViTnVtIjpbMl0sImlkIjoiMDAwMDAwMDA1IiwibmFtZSI6IuuLpOyEryIsInByb2ZpbGVQYXRoIjpudWxsLCJpc0FkbWluIjowLCJpYXQiOjE2MzY2MzM4MjMsImV4cCI6MTYzNjcyMDIyMywiaXNzIjoid29vYWhhbiBhZ2lsZSJ9.nWkuNJp0Odo-y3on9VV18XDoTEgxvnuNOKyskPEM4Fg'
+        )
+        .set('Content-Type', 'application/json')
+        .send(createSchedule);
       expect(res.statusCode).toEqual(200);
     } catch (err) {
       console.log(err);
