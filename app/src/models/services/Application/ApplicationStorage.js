a8175d896790f31dd8c208392a1947c593bc5464
김지수 <kimjisu@gimjisuui-MacBookPro.local>
2021-11-19 13:18:21 +0900

    Refacot(jisu/notification): 동아리 관리자 조회 쿼리문 수정

--- app/src/models/services/Application/ApplicationStorage.js
+++ app/src/models/services/Application/ApplicationStorage.js
@@ -211,20 +211,28 @@ class ApplicationStorage {
         FROM students AS s JOIN applicants AS app ON app.club_no = ?
         AND app.student_id = s.id AND app.reading_flag = 0;`;
 
-      const questionAnswerQuery = `SELECT app.student_id AS id, GROUP_CONCAT(q.description) AS question, GROUP_CONCAT(a.description) AS answer 
-        FROM answers AS a 
-        JOIN applicants AS app 
-        ON a.student_id = app.student_id AND app.club_no = 2 AND app.reading_flag = 0 
-        JOIN questions AS q 
-        ON a.question_no = q.no AND app.club_no = q.club_no 
-        GROUP BY app.student_id;`;
+      const questionAnswerQuery = `SELECT app.student_id AS id, q.description AS question, a.description AS answer
+      FROM applicants AS app JOIN answers AS a ON a.student_id = app.student_id
+      AND app.club_no = 2 AND app.reading_flag = 0 JOIN questions AS q
+      ON a.question_no = q.no AND app.club_no = q.club_no;`;
+
+      const applicantQuery = `SELECT student_id AS id FROM applicants WHERE reading_flag = 0 AND club_no = ?;`;
 
       const applicantInfo = await conn.query(applicantInfoQuery, clubNum);
-      const questionsAnswers = await conn.query(questionAnswerQuery, clubNum);
+      const qAndA = await conn.query(questionAnswerQuery, clubNum);
+      const applicants = await conn.query(applicantQuery, clubNum);
 
-      for (let i = 0; i < questionsAnswers.length; i += 1) {
-        questionsAnswers[i].question = questionsAnswers[i].question.split(',');
-        questionsAnswers[i].answer = questionsAnswers[i].answer.split(',');
+      const questionsAnswers = [[]];
+      for (let i = 0; i < applicants.length; i += 1) {
+        const studentId = applicants[i].id;
+
+        for (let j = 0; j < qAndA.length; j += 1) {
+          if (studentId === qAndA[j].id) {
+            questionsAnswers[i].push(qAndA[j]);
+          }
+        }
+        if (i === applicants.length - 1) break;
+        else questionsAnswers.push([]);
       }
 
       return {
