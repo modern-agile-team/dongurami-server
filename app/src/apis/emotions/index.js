ea69c72c336a4c9ce8372ec26100460139291e77
lsh <rrgks@naver.com>
2021-11-25 17:37:16 +0900

    Refactor(seokho/profile): 커밋내역 분리 #370

--- app/src/apis/emotions/index.js
+++ app/src/apis/emotions/index.js
@@ -3,43 +3,36 @@
 const express = require('express');
 const loginAuth = require('../../middlewares/login-auth');
 const ctrl = require('./emotion.ctrl');
-const apiAuth = require('../../middlewares/api-auth');
 
 const router = express.Router();
 
 router.patch(
   '/liked/board/:boardNum',
-  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.likedByBoardNum
 );
 router.patch(
   '/liked/comment/:cmtNum',
-  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.likedByCmtNum
 );
 router.patch(
   '/liked/reply-comment/:replyCmtNum',
-  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.likedByReplyCmtNum
 );
 router.patch(
   '/unliked/board/:boardNum',
-  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.unLikedByBoardNum
 );
 router.patch(
   '/unliked/comment/:cmtNum',
-  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.unLikedByCmtNum
 );
 router.patch(
   '/unliked/reply-comment/:replyCmtNum',
-  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.unLikedByReplyCmtNum
 );
