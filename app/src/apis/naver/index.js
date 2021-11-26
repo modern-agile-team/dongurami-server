2f04df85a96caa6e1dd850ac7b84111aa5d3ffd5
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-11-26 11:42:43 +0900

    Add(changhoon/naver) : naver api-auth 미들웨어 추가 #370

--- app/src/apis/naver/index.js
+++ app/src/apis/naver/index.js
@@ -5,8 +5,14 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('./naver.ctrl');
 const snsTokenAuth = require('../../middlewares/snsToken-auth');
+const apiAuth = require('../../middlewares/api-auth');
 
-router.get('/login', snsTokenAuth.snsTokenCheck, ctrl.process.login);
-router.post('/sign-up', ctrl.process.signUp);
+router.get(
+  '/login',
+  apiAuth.apiAuth,
+  snsTokenAuth.snsTokenCheck,
+  ctrl.process.login
+);
+router.post('/sign-up', apiAuth.apiAuth, ctrl.process.signUp);
 
 module.exports = router;
