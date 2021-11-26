2f73e8e4e6d6814aedd63c5b1ef7c3c2822c8c41
김지수 <kimjisu@gimjisuui-MacBookPro.local>
2021-11-26 10:22:00 +0900

    Add(jisu/notification): apiAuth 미들웨어 적용

--- app/src/apis/admin-option/index.js
+++ app/src/apis/admin-option/index.js
@@ -6,9 +6,11 @@ const router = express.Router();
 const ctrl = require('./admin-option.ctrl');
 const loginAuth = require('../../middlewares/login-auth');
 const clubAuth = require('../../middlewares/club-auth');
+const apiAuth = require('../../middlewares/api-auth');
 
 router.get(
   '/:clubNum',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   clubAuth.clubJoinCheck,
   ctrl.process.checkClubAdmin,
@@ -17,30 +19,35 @@ router.get(
 
 router.post(
   '/:clubNum/applicant',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.createMemberById
 );
 
 router.put(
   '/:clubNum/leader',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.updateLeaderById
 );
 
 router.put(
   '/:clubNum/admin-functions',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.updateAdminOptionById
 );
 
 router.put(
   '/:clubNum/applicant',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.updateApplicantById
 );
 
 router.delete(
   '/:clubNum/:memberId',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.deleteMemberById
 );
--- app/src/apis/notification/index.js
+++ app/src/apis/notification/index.js
@@ -5,15 +5,27 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('./notification.ctrl');
 const loginAuth = require('../../middlewares/login-auth');
+const apiAuth = require('../../middlewares/api-auth');
 
-router.get('/entire', loginAuth.loginCheck, ctrl.process.findAllById);
+router.get(
+  '/entire',
+  apiAuth.apiAuth,
+  loginAuth.loginCheck,
+  ctrl.process.findAllById
+);
 
 router.patch(
   '/:notificationNum',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.updateOneByNotificationNum
 );
 
-router.put('/entire', loginAuth.loginCheck, ctrl.process.updateAllById);
+router.put(
+  '/entire',
+  apiAuth.apiAuth,
+  loginAuth.loginCheck,
+  ctrl.process.updateAllById
+);
 
 module.exports = router;
