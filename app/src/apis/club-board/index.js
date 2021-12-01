71684b0cda6626972b81f91ec1d090c631c85ec1
rugaheedori <qufslarkgml@gmail.com>
2021-12-01 15:00:34 +0900

    Refactor(gahee/club-board) : apiAuth 미들웨어 수정 #116

--- app/src/apis/club-board/index.js
+++ app/src/apis/club-board/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const boardCtrl = require('../boards/board.ctrl');
 const commentCtrl = require('../boards/comment.ctrl');
 const myPageCtrl = require('../my-page/my-page.ctrl');
-// const apiAuth = require('../../middlewares/api-auth');
+const apiAuth = require('../../middlewares/api-auth');
 const loginCheck = require('../../middlewares/login-auth');
 const clubJoinCheck = require('../../middlewares/club-auth');
 
@@ -12,27 +12,32 @@ const router = express.Router();
 
 router.get(
   '/:category/:clubNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   boardCtrl.process.findAllByCategoryNum
 );
 router.get(
   '/:category/:clubNum/:boardNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   boardCtrl.process.findOneByBoardNum
 );
 
 router.post(
   '/:category/:clubNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   boardCtrl.process.createBoardNum
 );
 router.post(
   '/:category/:clubNum/:boardNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   commentCtrl.process.createCommentNum
 );
 router.post(
   '/:category/:clubNum/:boardNum/:cmtNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   commentCtrl.process.createReplyCommentNum
 );
@@ -40,6 +45,7 @@ router.post(
 // 동아리 별 활동 일지 스크랩 요청 API
 router.post(
   '/:category/personal/scrap/:clubNum/:boardNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   clubJoinCheck.clubJoinCheck,
   myPageCtrl.process.createScrapNum
@@ -47,32 +53,38 @@ router.post(
 
 router.put(
   '/:category/:clubNum/:boardNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   boardCtrl.process.updateOneByBoardNum
 );
 router.put(
   '/:category/:clubNum/:boardNum/:cmtNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   commentCtrl.process.updateByCommentNum
 );
 router.put(
   '/:category/:clubNum/:boardNum/:cmtNum/:replyCmtNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   commentCtrl.process.updateByReplyCommentNum
 );
 
 router.delete(
   '/:category/:clubNum/:boardNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   boardCtrl.process.deleteOneByBoardNum
 );
 router.delete(
   '/:category/:clubNum/:boardNum/:cmtNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   commentCtrl.process.deleteAllByGroupNum
 );
 router.delete(
   '/:category/:clubNum/:boardNum/:cmtNum/:replyCmtNum',
+  apiAuth.apiAuth,
   loginCheck.loginCheck,
   commentCtrl.process.deleteOneReplyCommentNum
 );
