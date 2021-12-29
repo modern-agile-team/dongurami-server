d8f94395c367b70d07ba30932abd7d35797eb470
lsh <rrgks@naver.com>
2021-12-29 11:52:22 +0900

    Refactor(seokho/emotion): index.js 다른 연결파일들과 같이 router 순서 변경 및 줄바꿈 추가 #428

--- app/src/apis/emotion/index.js
+++ app/src/apis/emotion/index.js
@@ -14,28 +14,30 @@ router.patch(
   ctrl.process.likedByBoardNum
 );
 router.patch(
+  '/unliked/board/:boardNum',
+  apiAuth.apiAuth,
+  loginAuth.loginCheck,
+  ctrl.process.unLikedByBoardNum
+);
+
+router.patch(
   '/liked/comment/:cmtNum',
   apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.likedByCmtNum
 );
 router.patch(
-  '/liked/reply-comment/:replyCmtNum',
-  apiAuth.apiAuth,
-  loginAuth.loginCheck,
-  ctrl.process.likedByReplyCmtNum
-);
-router.patch(
-  '/unliked/board/:boardNum',
-  apiAuth.apiAuth,
-  loginAuth.loginCheck,
-  ctrl.process.unLikedByBoardNum
-);
-router.patch(
   '/unliked/comment/:cmtNum',
   apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.unLikedByCmtNum
+);
+
+router.patch(
+  '/liked/reply-comment/:replyCmtNum',
+  apiAuth.apiAuth,
+  loginAuth.loginCheck,
+  ctrl.process.likedByReplyCmtNum
 );
 router.patch(
   '/unliked/reply-comment/:replyCmtNum',
