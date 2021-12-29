fdaad5e186c1c4ab1cf9cdb65ed53675a57a2652
lsh <rrgks@naver.com>
2021-12-29 11:48:55 +0900

    Add(seokho/emotion): index.js apiAuth 미들웨어 추가 #428

--- app/src/apis/emotion/index.js
+++ app/src/apis/emotion/index.js
@@ -2,37 +2,44 @@
 
 const express = require('express');
 const loginAuth = require('../../middlewares/login-auth');
+const apiAuth = require('../../middlewares/api-auth');
 const ctrl = require('./emotion.ctrl');
 
 const router = express.Router();
 
 router.patch(
   '/liked/board/:boardNum',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.likedByBoardNum
 );
 router.patch(
   '/liked/comment/:cmtNum',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.likedByCmtNum
 );
 router.patch(
   '/liked/reply-comment/:replyCmtNum',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.likedByReplyCmtNum
 );
 router.patch(
   '/unliked/board/:boardNum',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.unLikedByBoardNum
 );
 router.patch(
   '/unliked/comment/:cmtNum',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.unLikedByCmtNum
 );
 router.patch(
   '/unliked/reply-comment/:replyCmtNum',
+  apiAuth.apiAuth,
   loginAuth.loginCheck,
   ctrl.process.unLikedByReplyCmtNum
 );
