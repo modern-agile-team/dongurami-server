f49c448be26e1eb4c0704b6ca63edbc449db8811
lsh <rrgks@naver.com>
2022-01-02 23:50:48 +0900

    Refactor(seokho/emotion): makeResponseByStatusCode 함수 리펙토링 #427

--- app/src/models/services/Emotion/utils.js
+++ app/src/models/services/Emotion/utils.js
@@ -27,45 +27,44 @@ class EmotionUtil {
   }
 
   static makeResponseByStatusCode(req, status) {
-    function makeReturnMsg() {
-      const apiPath = req.url.slice(1).split('/');
-      const target = {
-        board: '게시글',
-        comment: '댓글',
-        'reply-comment': '답글',
-      };
-      const failContent = {
-        liked: '실패했습니다.',
-        unliked: '취소되지 않았습니다.',
-      };
-      const passContent = {
-        liked: '',
-        unliked: '취소 ',
-      };
+    const apiPath = req.url.slice(1).split('/');
+    const target = {
+      board: '게시글',
+      comment: '댓글',
+      'reply-comment': '답글',
+    };
 
-      if (status === 409) {
-        if (apiPath[0] === 'liked') {
-          return '이미 좋아요를 눌렀습니다.';
-        }
-        return '좋아요를 누르지 않았습니다.';
-      }
-      if (status === 404) {
-        return `해당 ${target[apiPath[1]]}이 존재하지 않습니다`;
+    function makeLikedReturnMsg() {
+      if (status === 200) {
+        return `해당 ${target[apiPath[1]]}에 좋아요를 했습니다.`;
       }
       if (status === 400) {
-        return `해당 ${target[apiPath[1]]}에 좋아요가 ${
-          failContent[apiPath[0]]
-        }`;
+        return `해당 ${target[apiPath[1]]}에 좋아요를 실패 했습니다.`;
       }
-      return `해당 ${target[apiPath[1]]}에 좋아요를 ${
-        passContent[apiPath[0]]
-      }했습니다.`;
+      if (status === 404) {
+        return `해당 ${target[apiPath[1]]}이 존재하지 않습니다.`;
+      }
+      return `해당 ${target[apiPath[1]]}에 이미 좋아요를 눌렀습니다.`;
+    }
+
+    function makeUnlikedReturnMsg() {
+      if (status === 200) {
+        return `해당 ${target[apiPath[1]]}에 좋아요가 취소 되었습니다.`;
+      }
+      if (status === 400) {
+        return `해당 ${target[apiPath[1]]}에 좋아요가 취소되지 않았습니다.`;
+      }
+      if (status === 404) {
+        return `해당 ${target[apiPath[1]]}이 존재하지 않습니다.`;
+      }
+      return `해당 ${target[apiPath[1]]}에 좋아요를 누르지 않았습니다.`;
     }
 
     return {
       status,
       success: status === 200,
-      msg: makeReturnMsg(),
+      msg:
+        apiPath[0] === 'liked' ? makeLikedReturnMsg() : makeUnlikedReturnMsg(),
     };
   }
 }
