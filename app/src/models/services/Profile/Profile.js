84983861cb41fdd512c7992ac29a798704177e7a
lsh <rrgks@naver.com>
2022-01-04 13:55:42 +0900

    Refactor(seokho/profile): 정규표현식 변수 대문자표기법으로 변경 #439

--- app/src/models/services/Profile/Profile.js
+++ app/src/models/services/Profile/Profile.js
@@ -57,14 +57,14 @@ class Profile {
       profileImageUrl: request.profileImageUrl,
       userId: user.id,
     };
-    const emailRegExp =
+    const EMAILREGEXP =
       /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-    const phoneNumberRegExp = /[eE]/;
+    const PHONENUMREGEXP = /[eE]/;
     let msg = '';
 
     if (this.params.studentId !== userInfo.userId) {
       msg = '로그인된 사람의 프로필이 아닙니다.';
-    } else if (userInfo.email && userInfo.email.match(emailRegExp) === null) {
+    } else if (userInfo.email && userInfo.email.match(EMAILREGEXP) === null) {
       msg = '이메일 형식이 맞지 않습니다.';
     } else if (userInfo.email.length === 0) {
       msg = '이메일 형식이 맞지 안습니다.';
@@ -72,7 +72,7 @@ class Profile {
       userInfo.phoneNumber &&
       (userInfo.phoneNumber.length !== 11 ||
         Number.isNaN(Number(userInfo.phoneNumber)) ||
-        !(userInfo.phoneNumber.match(phoneNumberRegExp) === null))
+        !(userInfo.phoneNumber.match(PHONENUMREGEXP) === null))
     ) {
       msg = '전화번호 형식이 맞지 않습니다.';
     } else if (userInfo.phoneNumber === 0) {
