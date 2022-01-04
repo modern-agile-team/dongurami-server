576245fdd8f3f7fa08d060d15e874a3099614da0
lsh <rrgks@naver.com>
2022-01-04 11:55:42 +0900

    Refactor(seokho/profile): 각 return에 http 상태코드 추가 #439

--- app/src/models/services/Profile/Profile.js
+++ app/src/models/services/Profile/Profile.js
@@ -104,7 +104,7 @@ class Profile {
     } else if (userInfo.phoneNumber === 0) {
       msg = '이메일 형식이 맞지 않습니다.';
     }
-    if (msg) return { success: false, msg };
+    if (msg) return { success: false, msg, status: 400 };
 
     try {
       const snsUserInfo = await StudentStorage.findOneSnsUserById(user.id);
@@ -117,13 +117,18 @@ class Profile {
         return {
           success: false,
           msg: '네이버 이메일로 가입된 회원은 이메일 변경이 불가능합니다.',
+          status: 403,
         };
       }
 
       const isEmail = await StudentStorage.findOneByEmail(userInfo.email);
 
       if (isEmail && isEmail.id !== user.id) {
-        return { success: false, msg: '다른 유저가 사용중인 이메일입니다.' };
+        return {
+          success: false,
+          msg: '다른 유저가 사용중인 이메일입니다.',
+          status: 409,
+        };
       }
 
       const isPhoneNum = await StudentStorage.findOneByPhoneNum(
@@ -132,22 +137,30 @@ class Profile {
       );
 
       if (isPhoneNum) {
-        return { success: false, msg: '다른 유저가 사용중인 번호입니다.' };
+        return {
+          success: false,
+          msg: '다른 유저가 사용중인 번호입니다.',
+          status: 409,
+        };
       }
 
       const studentUpdateCnt = await ProfileStorage.updateStudentInfo(userInfo);
 
       if (studentUpdateCnt === 0) {
-        return { success: false, msg: '존재하지 않는 회원입니다.' };
+        return {
+          success: false,
+          msg: '존재하지 않는 회원입니다.',
+          status: 404,
+        };
       }
       if (userInfo.profileImageUrl !== user.profilePath) {
         const checkedId = await StudentStorage.findOneById(user.id);
         const clubs = await StudentStorage.findOneByLoginedId(user.id);
         const jwt = await Auth.createJWT(checkedId, clubs);
 
-        return { success: true, msg: '회원정보 수정 성공', jwt };
+        return { success: true, msg: '회원정보 수정 성공', status: 200, jwt };
       }
-      return { success: true, msg: '회원정보 수정 성공' };
+      return { success: true, msg: '회원정보 수정 성공', status: 200 };
     } catch (err) {
       return Error.ctrl('서버 에러입니다. 서버 개발자에게 얘기해주세요.', err);
     }
