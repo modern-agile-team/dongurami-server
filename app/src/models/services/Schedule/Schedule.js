6d287cda66d9517e8077d9069376eddc450c9e9d
rugaheedori <qufslarkgml@gmail.com>
2022-01-05 14:20:44 +0900

    Refactor(gahee/schedule) : 주요 일정 등록 불필요한 변수 수정 및 return문 makeMsg 함수로 변환 #442

--- app/src/models/services/Schedule/Schedule.js
+++ app/src/models/services/Schedule/Schedule.js
@@ -109,7 +109,7 @@ class Schedule {
         colorCode: data.colorCode,
         title: data.title,
         startDate: data.startDate,
-        endDate: data.endDate, // 수정하는 사람 =/= 작성자 가능성O => 학생 정보는 수정시 받지 X
+        endDate: data.endDate,
       };
       const success = await ScheduleStorage.updateSchedule(scheduleInfo);
 
@@ -133,30 +133,27 @@ class Schedule {
         // });
         return Schedule.makeMsg(200, '일정이 수정되었습니다.');
       }
-      return Schedule.makeMsg(400, '일정 수정에 실패하였습니다.');
+      return Schedule.makeMsg(400, '일정이 수정되지 않았습니다.');
     } catch (err) {
       return Error.ctrl('', err);
     }
   }
 
   async updateOnlyImportant() {
-    const data = this.body;
-    const { no } = this.params;
-
     try {
       const scheduleInfo = {
-        no,
-        important: data.important,
+        no: this.params.no,
+        important: this.body.important,
       };
 
       const success = await ScheduleStorage.updateOnlyImportant(scheduleInfo);
 
       if (success) {
-        return { success: true, msg: '주요 일정으로 등록되었습니다.' };
+        return Schedule.makeMsg(200, '주요 일정으로 등록되었습니다.');
       }
-      return { success: false, msg: '주요 일정 등록이되지 않았습니다.' };
+      return Schedule.makeMsg(400, '주요 일정으로 등록되지 않았습니다.');
     } catch (err) {
-      return Error.ctrl('개발자에게 문의해주세요.', err);
+      return Error.ctrl('', err);
     }
   }
 
