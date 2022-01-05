9d6360da6f4d88cb13514de5dc4b2c6eb4bb4ffc
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-01-05 14:46:25 +0900

    Refactor(changhoon/login) : 미사용 쿼리문 제거  #434

--- app/src/models/services/Student/StudentStorage.js
+++ app/src/models/services/Student/StudentStorage.js
@@ -99,30 +99,6 @@ class StudentStorage {
     }
   }
 
-  static async findOneByIdAndEmail(clientInfo) {
-    let conn;
-
-    try {
-      conn = await mariadb.getConnection();
-
-      const query = `
-        SELECT id, email
-        FROM students
-        WHERE id = ? OR email = ?;`;
-
-      const idEmailList = await conn.query(query, [
-        clientInfo.id,
-        clientInfo.email,
-      ]);
-
-      return idEmailList[0];
-    } catch (err) {
-      throw err;
-    } finally {
-      conn?.release();
-    }
-  }
-
   static async findOneByLoginedId(studentId) {
     let conn;
 
