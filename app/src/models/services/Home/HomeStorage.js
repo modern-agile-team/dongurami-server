bd11cfdffd4aaa6541e528aa1b2c2815887510c3
rugaheedori <qufslarkgml@gmail.com>
2022-01-07 16:07:05 +0900

    Modify(gahee/circle=home) : 동아리 정보 수정, 동아리 로고 수정 시 clubAuth.clubJoinCheck 미들웨어 추가 및 query문 컨벤션 수정 #428

--- app/src/models/services/Home/HomeStorage.js
+++ app/src/models/services/Home/HomeStorage.js
@@ -9,10 +9,13 @@ class HomeStorage {
     try {
       conn = await mariadb.getConnection();
 
-      const query = `SELECT s.id, s.name, s.profile_image_url AS profileImageUrl FROM students AS s
-        LEFT JOIN clubs AS c ON c.leader = s.id WHERE c.no = ?;`;
+      const query = `
+        SELECT s.id, s.name, s.profile_image_url AS profileImageUrl 
+        FROM students AS s
+        LEFT JOIN clubs AS c 
+        ON c.leader = s.id WHERE c.no = ?;`;
 
-      const leader = await conn.query(query, clubNum);
+      const leader = await conn.query(query, [clubNum]);
 
       return leader[0];
     } catch (err) {
@@ -28,8 +31,10 @@ class HomeStorage {
     try {
       conn = await mariadb.getConnection();
 
-      const findFlag =
-        'SELECT join_admin_flag AS joinAdminFlag, board_admin_flag AS boardAdminFlag FROM members WHERE student_id = ?;';
+      const findFlag = `
+        SELECT join_admin_flag AS joinAdminFlag, board_admin_flag AS boardAdminFlag 
+        FROM members 
+        WHERE student_id = ?;`;
 
       const flags = await conn.query(findFlag, ids.clientId);
       const clientInfo = {};
@@ -52,8 +57,10 @@ class HomeStorage {
     try {
       conn = await mariadb.getConnection();
 
-      const findClubInfo =
-        'SELECT name, category, logo_url AS logoUrl, introduce FROM clubs WHERE no = ?;';
+      const findClubInfo = `
+        SELECT name, category, logo_url AS logoUrl, introduce 
+        FROM clubs 
+        WHERE no = ?;`;
 
       const clubInfo = await conn.query(findClubInfo, clubNum);
 
@@ -71,8 +78,9 @@ class HomeStorage {
     try {
       conn = await mariadb.getConnection();
 
-      const query = `SELECT SUM(M) AS man, SUM(W) AS women FROM
-        (SELECT (CASE gender WHEN 1 THEN 1 ELSE 0 END) AS M, 
+      const query = `
+        SELECT SUM(M) AS man, SUM(W) AS women 
+        FROM (SELECT (CASE gender WHEN 1 THEN 1 ELSE 0 END) AS M, 
         (CASE gender WHEN 2 THEN 1 ELSE 0 END) AS W 
         FROM (SELECT gender FROM students INNER JOIN members ON students.id = members.student_id WHERE club_no = ?) 
         AS collectMember) AS collectGender;`;
@@ -93,7 +101,10 @@ class HomeStorage {
     try {
       conn = await mariadb.getConnection();
 
-      const query = `SELECT leader FROM clubs WHERE no = ?;`;
+      const query = `
+        SELECT leader 
+        FROM clubs 
+        WHERE no = ?;`;
 
       const leader = await conn.query(query, id);
 
@@ -111,7 +122,10 @@ class HomeStorage {
     try {
       conn = await mariadb.getConnection();
 
-      const query = `UPDATE clubs SET introduce = ? WHERE no = ?;`;
+      const query = `
+        UPDATE clubs 
+        SET introduce = ? 
+        WHERE no = ?;`;
 
       await conn.query(query, [clubInfo.introduce, clubInfo.clubNum]);
 
@@ -129,7 +143,10 @@ class HomeStorage {
     try {
       conn = await mariadb.getConnection();
 
-      const query = `UPDATE clubs SET logo_url = ? WHERE no = ?;`;
+      const query = `
+        UPDATE clubs 
+        SET logo_url = ? 
+        WHERE no = ?;`;
 
       await conn.query(query, [logoInfo.logoUrl, logoInfo.clubNum]);
 
