28a8b0645b003d0028879dae0831f0209e6deb79
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-01-07 18:07:21 +0900

    Delete(changhoon/sign-up) : 회원가입 검증함수 쪼개기 #452

--- app/src/models/services/Student/SignUpCheck.js
+++ app/src/models/services/Student/SignUpCheck.js
@@ -5,32 +5,52 @@ class SignUpCheck {
     return {
       success: status < 400,
       status,
-      msg,
+      msg: `${msg}형식을 확인해주세요.`,
     };
+  }
+
+  static idCheck(infoCategory) {
+    const ID_REGEXP = /^\d{9}$/;
+    return ID_REGEXP.test(infoCategory[0]);
+  }
+
+  static passwordCheck(infoCategory) {
+    const PASSWORD_REGEXP = /^[0-9a-zA-Z$@$!%*#?&]{8,}$/;
+    return PASSWORD_REGEXP.test(infoCategory[1]);
+  }
+
+  static nameCheck(infoCategory) {
+    const NAME_REGEXP = /^[가-힣-a-zA-Z]+$/;
+    return NAME_REGEXP.test(infoCategory[2]);
+  }
+
+  static emailCheck(infoCategory) {
+    const EMAIL_REGEXP =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    return EMAIL_REGEXP.test(infoCategory[3]);
+  }
+
+  static majorCheck(infoCategory) {
+    return infoCategory[4];
   }
 
   static infoCheck(saveInfo) {
     const infoCategory = Object.values(saveInfo);
-    const ID_REGEXP = /^\d{9}$/;
-    const PASSWORD_REGEXP = /^[0-9a-zA-Z$@$!%*#?&]{8,}$/;
-    const NAME_REGEXP = /^[가-힣-a-zA-Z]+$/;
-    const EMAIL_REGEXP =
-      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 
-    if (!infoCategory[0].match(ID_REGEXP)) {
-      return SignUpCheck.makeResponseMsg(400, '아이디 형식이 맞지 않습니다.');
+    if (!SignUpCheck.idCheck(infoCategory)) {
+      return SignUpCheck.makeResponseMsg(400, '아이디');
     }
-    if (!infoCategory[1].match(PASSWORD_REGEXP)) {
-      return SignUpCheck.makeResponseMsg(400, '비밀번호 형식이 맞지 않습니다.');
+    if (!SignUpCheck.passwordCheck(infoCategory)) {
+      return SignUpCheck.makeResponseMsg(400, '비밀번호');
     }
-    if (!infoCategory[2].match(NAME_REGEXP)) {
-      return SignUpCheck.makeResponseMsg(400, '이름 형식이 맞지 않습니다.');
+    if (!SignUpCheck.nameCheck(infoCategory)) {
+      return SignUpCheck.makeResponseMsg(400, '이름');
     }
-    if (!infoCategory[3].match(EMAIL_REGEXP)) {
-      return SignUpCheck.makeResponseMsg(400, '이메일 형식이 맞지 않습니다.');
+    if (!SignUpCheck.emailCheck(infoCategory)) {
+      return SignUpCheck.makeResponseMsg(400, '이메일');
     }
-    if (!infoCategory[4]) {
-      return SignUpCheck.makeResponseMsg(400, '학과가 선택되지 않았습니다.');
+    if (!SignUpCheck.majorCheck(infoCategory)) {
+      return SignUpCheck.makeResponseMsg(400, '학과');
     }
     return { success: true };
   }
