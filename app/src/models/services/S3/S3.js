3f0445e47299596effa871f39f1d27b21385abe9
lsh <rrgks@naver.com>
2022-01-10 16:47:03 +0900

    Refactor(seokho/S3): 성공 return makeResponse 적용 #427

--- app/src/models/services/S3/S3.js
+++ app/src/models/services/S3/S3.js
@@ -36,12 +36,7 @@ class S3 {
         Key: img,
       });
 
-      return {
-        success: true,
-        msg: 'url 생성 성공',
-        preSignedPutUrl,
-        readObjectUrl,
-      };
+      return makeResponse(200, 'url 생성 성공', preSignedPutUrl, readObjectUrl);
     } catch (err) {
       return Error.ctrl('서버 에러입니다. 서버 개발자에게 얘기해주세요', err);
     }
