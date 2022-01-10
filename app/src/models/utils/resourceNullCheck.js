f4ccd7dfd072c20df80c7cbed4ef5618bba95701
lsh <rrgks@naver.com>
2022-01-10 16:34:06 +0900

    Refactor(seokho/S3): resourceNullCheck msg 생성 #472

--- app/src/models/utils/resourceNullCheck.js
+++ app/src/models/utils/resourceNullCheck.js
@@ -3,7 +3,9 @@
 function resourceNullCheck(resource) {
   for (const key in resource) {
     if (Object.prototype.hasOwnProperty.call(resource, key)) {
-      if (!resource[key]) return { success: true, status: 400, msg: `` };
+      if (!resource[key]) {
+        return { success: true, status: 400, msg: `${key} is not null` };
+      }
     }
   }
   return { success: false };
