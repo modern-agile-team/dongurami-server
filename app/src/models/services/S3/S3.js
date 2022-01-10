3479e4f2d296311394b215174b45c63dbee3b11e
lsh <rrgks@naver.com>
2022-01-10 16:47:50 +0900

    Delete(seokho/S3): Error.ctrl clientMsg 제거 #472

--- app/src/models/services/S3/S3.js
+++ app/src/models/services/S3/S3.js
@@ -38,7 +38,7 @@ class S3 {
 
       return makeResponse(200, 'url 생성 성공', preSignedPutUrl, readObjectUrl);
     } catch (err) {
-      return Error.ctrl('서버 에러입니다. 서버 개발자에게 얘기해주세요', err);
+      return Error.ctrl('', err);
     }
   }
 }
