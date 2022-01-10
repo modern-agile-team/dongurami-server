4db270e9b0114f784b62ab51e028fed88e049e50
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-01-10 11:45:31 +0900

    Refactor(changhoon/sign-up) : 줄바꿈 컨벤션 && 매개변수 변경 #452

--- app/src/models/services/Student/SignUpCheck.js
+++ app/src/models/services/Student/SignUpCheck.js
@@ -9,47 +9,51 @@ class SignUpCheck {
     };
   }
 
-  static idCheck(infoCategory) {
+  static idCheck(id) {
     const ID_REGEXP = /^\d{9}$/;
-    return ID_REGEXP.test(infoCategory[0]);
+
+    return ID_REGEXP.test(id);
   }
 
-  static passwordCheck(infoCategory) {
+  static passwordCheck(password) {
     const PASSWORD_REGEXP = /^[0-9a-zA-Z$@$!%*#?&]{8,}$/;
-    return PASSWORD_REGEXP.test(infoCategory[1]);
+
+    return PASSWORD_REGEXP.test(password);
   }
 
-  static nameCheck(infoCategory) {
+  static nameCheck(name) {
     const NAME_REGEXP = /^[가-힣-a-zA-Z]+$/;
-    return NAME_REGEXP.test(infoCategory[2]);
+
+    return NAME_REGEXP.test(name);
   }
 
-  static emailCheck(infoCategory) {
+  static emailCheck(email) {
     const EMAIL_REGEXP =
       /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-    return EMAIL_REGEXP.test(infoCategory[3]);
+
+    return EMAIL_REGEXP.test(email);
   }
 
-  static majorCheck(infoCategory) {
-    return infoCategory[4];
+  static majorCheck(major) {
+    return major;
   }
 
   static infoCheck(saveInfo) {
     const infoCategory = Object.values(saveInfo);
 
-    if (!SignUpCheck.idCheck(infoCategory)) {
+    if (!SignUpCheck.idCheck(infoCategory[0])) {
       return SignUpCheck.makeResponseMsg(400, '아이디');
     }
-    if (!SignUpCheck.passwordCheck(infoCategory)) {
+    if (!SignUpCheck.passwordCheck(infoCategory[1])) {
       return SignUpCheck.makeResponseMsg(400, '비밀번호');
     }
-    if (!SignUpCheck.nameCheck(infoCategory)) {
+    if (!SignUpCheck.nameCheck(infoCategory[2])) {
       return SignUpCheck.makeResponseMsg(400, '이름');
     }
-    if (!SignUpCheck.emailCheck(infoCategory)) {
+    if (!SignUpCheck.emailCheck(infoCategory[3])) {
       return SignUpCheck.makeResponseMsg(400, '이메일');
     }
-    if (!SignUpCheck.majorCheck(infoCategory)) {
+    if (!SignUpCheck.majorCheck(infoCategory[4])) {
       return SignUpCheck.makeResponseMsg(400, '학과');
     }
     return { success: true };
