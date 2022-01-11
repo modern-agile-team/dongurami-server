8cb31aa2dc6eccec4df50a0f19b53f5aac1a69ea
lsh <rrgks@naver.com>
2022-01-11 16:57:35 +0900

    Refactor(seokho/identify-auth): 주석 제거 #485

--- app/src/middlewares/identify-auth.js
+++ app/src/middlewares/identify-auth.js
@@ -7,7 +7,6 @@ const identityCheck = (req, res, next) => {
 
   if (token === 'null') token = '';
 
-  // token 존재 -> 로그인 / 임의토큰
   if (token) {
     loginCheck.loginCheck(req, res, next);
   } else next();
