7cc5e76049bb457a25135d67aad96d860c3a5092
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2022-01-11 17:11:14 +0900

    Refactor(changhoon/email) : 컨벤션 수정 및 return의 err 제거 #473

--- app/src/models/services/Auth/EmailAuth/EmailAuth.js
+++ app/src/models/services/Auth/EmailAuth/EmailAuth.js
@@ -30,7 +30,7 @@ class EmailAuth {
       const isSave = await EmailAuthStorage.saveToken(student);
 
       if (isSave) return { success: true, token };
-      return { success: false, err };
+      return { success: false };
     } catch (err) {
       return Error.ctrl(
         '알 수 없는 오류입니다. 서버개발자에게 문의하세요.',
@@ -43,9 +43,7 @@ class EmailAuth {
     try {
       const token = await EmailAuthStorage.findOneByStudentId(reqInfo.id);
 
-      if (token === reqInfo.token) {
-        return { success: true };
-      }
+      if (token === reqInfo.token) return { success: true };
       return EmailAuth.makeResponseMsg(
         403,
         '토큰이 유효하지 않거나 입력한 아이디와 일치하지 않습니다.'
