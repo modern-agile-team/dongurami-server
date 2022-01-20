96bcb49a3e26f5feab2c7e11e4687496c6c7a132
rugaheedori <qufslarkgml@gmail.com>
2022-01-20 15:14:18 +0900

    Refactor(gahee/my-page) : 스크랩한 글의 본문과 자신이 추가적으로 적은 본문 중 첫번째 이미지를 찾고 썸네일로 지정하는 extractThumbnail 함수 추출 및 상수 try문 밖으로 이동 #474

--- app/src/models/services/MyPage/MyPageUtils.js
+++ app/src/models/services/MyPage/MyPageUtils.js
@@ -1,5 +1,13 @@
 'use strict';
 
-class MyPageUtil {}
+class MyPageUtil {
+  static extractThumbnail(description) {
+    const IMG_REG = /<img[^>]*src=(["']?([^>"']+)["']?[^>]*)>/i;
+
+    IMG_REG.test(description);
+
+    return RegExp.$2;
+  }
+}
 
 module.exports = MyPageUtil;
