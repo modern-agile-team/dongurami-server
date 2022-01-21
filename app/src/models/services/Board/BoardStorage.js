4a67905827afac7185554916225e6da664aeaaec
lsh <rrgks@naver.com>
2022-01-21 15:59:19 +0900

    Delete(seokho/board): BoardStorage 필요없는 코드 제거 #493

--- app/src/models/services/Board/BoardStorage.js
+++ app/src/models/services/Board/BoardStorage.js
@@ -32,27 +32,6 @@ class BoardStorage {
     }
   }
 
-  static async existOnlyBoardNum(boardNum) {
-    let conn;
-
-    try {
-      conn = await mariadb.getConnection();
-
-      const query = `
-        SELECT no
-        FROM boards
-        WHERE no = ?;`;
-
-      const board = await conn.query(query, [boardNum]);
-
-      return board[0];
-    } catch (err) {
-      throw err;
-    } finally {
-      conn?.release();
-    }
-  }
-
   static async findBoardAdminFlag(clubNum, id) {
     let conn;
 
