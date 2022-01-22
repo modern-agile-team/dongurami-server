33d7a8c0344427b1465e9fc4606638d6af4207cc
rugaheedori <qufslarkgml@gmail.com>
2022-01-23 01:11:16 +0900

    Refactor(gahee/my-page) : DB query문 정렬 #474

--- app/src/models/services/MyPage/MyPageStorage.js
+++ app/src/models/services/MyPage/MyPageStorage.js
@@ -152,33 +152,6 @@ class MyPageStorage {
     }
   }
 
-  static async createScrapNum(scrapInfo) {
-    let conn;
-
-    try {
-      conn = await mariadb.getConnection();
-
-      const query = `
-        INSERT INTO scraps (student_id, club_no, title, scrap_description, board_description, file_url) 
-        VALUES (?, ?, ?, ?, ?, ?);`;
-
-      const scrap = await conn.query(query, [
-        scrapInfo.id,
-        scrapInfo.clubNum,
-        scrapInfo.title,
-        scrapInfo.scrapDescription,
-        scrapInfo.boardDescription,
-        scrapInfo.fileUrl,
-      ]);
-
-      return scrap.affectedRows;
-    } catch (err) {
-      throw err;
-    } finally {
-      conn?.release();
-    }
-  }
-
   static async findBoardDescription(scrapNum) {
     let conn;
 
@@ -193,51 +166,6 @@ class MyPageStorage {
       const board = await conn.query(query, [scrapNum]);
 
       return board[0].description;
-    } catch (err) {
-      throw err;
-    } finally {
-      conn?.release();
-    }
-  }
-
-  static async updateOneByScrapNum(scrapInfo) {
-    let conn;
-
-    try {
-      conn = await mariadb.getConnection();
-
-      const query = `
-        UPDATE scraps 
-        SET title = ?, scrap_description = ? , file_url = ? WHERE no = ?;`;
-
-      const scrap = await conn.query(query, [
-        scrapInfo.title,
-        scrapInfo.description,
-        scrapInfo.fileUrl,
-        scrapInfo.scrapNum,
-      ]);
-
-      return scrap.affectedRows;
-    } catch (err) {
-      throw err;
-    } finally {
-      conn?.release();
-    }
-  }
-
-  static async deleteOneByScrapNum(scrapNum) {
-    let conn;
-
-    try {
-      conn = await mariadb.getConnection();
-
-      const query = `
-      DELETE FROM scraps 
-      WHERE no = ?;`;
-
-      const scrap = await conn.query(query, [scrapNum]);
-
-      return scrap.affectedRows;
     } catch (err) {
       throw err;
     } finally {
@@ -262,48 +190,6 @@ class MyPageStorage {
       ]);
 
       return result[0];
-    } catch (err) {
-      throw err;
-    } finally {
-      conn?.release();
-    }
-  }
-
-  static async updateRejectedApplicant(userInfo) {
-    let conn;
-
-    try {
-      conn = await mariadb.getConnection();
-
-      const query = `
-        UPDATE applicants
-        UPDATE applicants 
-        SET reading_flag = 2 
-        WHERE club_no = ? AND student_id = ?;`;
-
-      const isUpdate = await conn.query(query, [userInfo.clubNum, userInfo.id]);
-
-      return isUpdate.affectedRows;
-    } catch (err) {
-      throw err;
-    } finally {
-      conn?.release();
-    }
-  }
-
-  static async deleteMemberById(userInfo) {
-    let conn;
-
-    try {
-      conn = await mariadb.getConnection();
-
-      const query = `
-        DELETE FROM members 
-        WHERE club_no = ? AND student_id = ?;`;
-
-      const isDelete = await conn.query(query, [userInfo.clubNum, userInfo.id]);
-
-      return isDelete.affectedRows;
     } catch (err) {
       throw err;
     } finally {
@@ -351,6 +237,120 @@ class MyPageStorage {
         clubs.push(clubList[i].clubNum);
       }
       return clubs;
+    } catch (err) {
+      throw err;
+    } finally {
+      conn?.release();
+    }
+  }
+
+  static async createScrapNum(scrapInfo) {
+    let conn;
+
+    try {
+      conn = await mariadb.getConnection();
+
+      const query = `
+        INSERT INTO scraps (student_id, club_no, title, scrap_description, board_description, file_url) 
+        VALUES (?, ?, ?, ?, ?, ?);`;
+
+      const scrap = await conn.query(query, [
+        scrapInfo.id,
+        scrapInfo.clubNum,
+        scrapInfo.title,
+        scrapInfo.scrapDescription,
+        scrapInfo.boardDescription,
+        scrapInfo.fileUrl,
+      ]);
+
+      return scrap.affectedRows;
+    } catch (err) {
+      throw err;
+    } finally {
+      conn?.release();
+    }
+  }
+
+  static async updateOneByScrapNum(scrapInfo) {
+    let conn;
+
+    try {
+      conn = await mariadb.getConnection();
+
+      const query = `
+        UPDATE scraps 
+        SET title = ?, scrap_description = ? , file_url = ? WHERE no = ?;`;
+
+      const scrap = await conn.query(query, [
+        scrapInfo.title,
+        scrapInfo.description,
+        scrapInfo.fileUrl,
+        scrapInfo.scrapNum,
+      ]);
+
+      return scrap.affectedRows;
+    } catch (err) {
+      throw err;
+    } finally {
+      conn?.release();
+    }
+  }
+
+  static async updateRejectedApplicant(userInfo) {
+    let conn;
+
+    try {
+      conn = await mariadb.getConnection();
+
+      const query = `
+        UPDATE applicants
+        UPDATE applicants 
+        SET reading_flag = 2 
+        WHERE club_no = ? AND student_id = ?;`;
+
+      const isUpdate = await conn.query(query, [userInfo.clubNum, userInfo.id]);
+
+      return isUpdate.affectedRows;
+    } catch (err) {
+      throw err;
+    } finally {
+      conn?.release();
+    }
+  }
+
+  static async deleteOneByScrapNum(scrapNum) {
+    let conn;
+
+    try {
+      conn = await mariadb.getConnection();
+
+      const query = `
+        DELETE FROM scraps 
+        WHERE no = ?;`;
+
+      const scrap = await conn.query(query, [scrapNum]);
+
+      return scrap.affectedRows;
+    } catch (err) {
+      throw err;
+    } finally {
+      conn?.release();
+    }
+  }
+
+  static async deleteMemberById(userInfo) {
+    let conn;
+
+    try {
+      conn = await mariadb.getConnection();
+
+      const query = `
+        DELETE FROM members 
+        WHERE club_no = ? AND student_id = ?;`;
+
+      const isDelete = await conn.query(query, [userInfo.clubNum, userInfo.id]);
+
+      return isDelete.affectedRows;
     } catch (err) {
       throw err;
     } finally {
