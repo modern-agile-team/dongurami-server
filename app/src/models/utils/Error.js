77cf22013b1f51d69a38fa80b9b39bb5135b6ff5
lsh <rrgks@naver.com>
2022-01-24 11:55:58 +0900

    Delete(seokho/board): Error 클레스 필요없는 함수 제거 #493

--- app/src/models/utils/Error.js
+++ app/src/models/utils/Error.js
@@ -8,15 +8,6 @@ class Error {
       clientMsg: '서버 에러입니다. 서버 개발자에게 문의해 주세요.',
     };
   }
-
-  static dbError() {
-    return {
-      isError: true,
-      errMsg: new Error('DB접근중 알수없는 에러'),
-      clientMsg:
-        'DB접근중 알수없는 에러가 발생했습니다. 서버 개발자에게 문의해 주세요',
-    };
-  }
 }
 
 module.exports = Error;
